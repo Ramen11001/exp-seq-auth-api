bb6d6065c0e5ae84cfcac1c2748f528c917db5ff
Solans <solanshnd@gmail.com>
2025-03-21 09:03:07 -0400

    [FIX] Se agregó el archivo .sequelizerc con las rutas de los modelos, migraciones y semillas.

---
+++ .sequelizerc.js
@@ -0,0 +1,7 @@
+module.exports ={
+"config": path.resolve('./src/config/sequelize-config.json'),
+"models": path.resolve('./src/models/indez.js'),
+"seeders": path.resolve('./src/seeders'),
+"migratiion": path.resolve('./src/migrations')
+
+};
