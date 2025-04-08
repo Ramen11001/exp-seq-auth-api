662ea5ea5c970c51ad899944e563d67eac0822c1
Ramen11001 <soltashnd@gmail.com>
2025-04-08 18:46:53 +0200

    [FTR] The new migration was performed, and it was made incremental to access the sequelize method changeColumn. A name issue was also detected in the FK in the product model. The migration does not run.

--- src/migrations/20250407144922-update-products.js
+++ src/migrations/20250407144922-update-products.js
@@ -3,20 +3,26 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+    await queryInterface.changeColumn('Products', 'description', {
+      type: Sequelize.STRING,
+      allowNull: true, // Change this
+    });
+
+    await queryInterface.changeColumn('Products', 'price', {
+      type: Sequelize.DOUBLE,
+      allowNull: false, // CChange this
+    });
   },
 
   async down (queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+    await queryInterface.changeColumn('Products', 'description', {
+      type: Sequelize.STRING,
+      allowNull: false, 
+    });
+
+    await queryInterface.changeColumn('Products', 'price', {
+      type: Sequelize.DOUBLE,
+      allowNull: true,
+    });
   }
 };
