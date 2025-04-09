8392fca71c4360b58f5ff3cfce3769ad4f42fe3f
Solans <solanshnd@gmail.com>
2025-04-09 09:04:52 -0400

    [FIX] Removed an unnecessary feature

--- src/migrations/20250407144922-update-products.js
+++ src/migrations/20250407144922-update-products.js
@@ -2,27 +2,17 @@
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
+  async up(queryInterface, Sequelize) {
     await queryInterface.changeColumn('Products', 'description', {
       type: Sequelize.STRING,
       allowNull: true, // Change this
     });
-
-    await queryInterface.changeColumn('Products', 'price', {
-      type: Sequelize.DOUBLE,
-      allowNull: false, // CChange this
-    });
   },
 
-  async down (queryInterface, Sequelize) {
+  async down(queryInterface, Sequelize) {
     await queryInterface.changeColumn('Products', 'description', {
       type: Sequelize.STRING,
-      allowNull: false, 
-    });
-
-    await queryInterface.changeColumn('Products', 'price', {
-      type: Sequelize.DOUBLE,
-      allowNull: true,
+      allowNull: false,
     });
   }
 };
