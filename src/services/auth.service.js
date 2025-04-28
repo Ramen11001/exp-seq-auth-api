904451d1927e843c1e39b8d132a7a855eb378437
Solans <solanshnd@gmail.com>
2025-04-28 11:09:17 -0400

    [FTR] was add auth.service.js, auth.js route, the auth middleware

---
+++ src/services/auth.service.js
@@ -0,0 +1,23 @@
+module.exports = router;
+const jwt = require("jsonwebtoken");
+const User = require("../models/user"); 
+
+const SECRET_KEY = "secret_key"; // Yo estoy usando md5 para encriptar el password. Tiene algo que ver? 
+
+/**
+ * User authentication and token generation
+ * @param {string} username - User's usernam
+ * @param {string} password - Password
+ * @returns {string} JWT token
+ */
+const login = async (username, password) => {
+    const user = await User.findOne({ where: { username } });
+  
+    if (!user || user.password !== password) {
+      throw new Error("Credenciales inválidas");
+    }
+  
+    return jwt.sign({ id: user.id, username: user.username }, SECRET_KEY, { expiresIn: "1h" });
+  };
+  
+  module.exports = { login };
