8bb899b4c196998c683a4e819d2b65f7b5cb5888
Ramen11001 <soltashnd@gmail.com>
2025-04-29 21:16:01 +0200

    [FX] Add paths

--- src/app.js
+++ src/app.js
@@ -7,6 +7,8 @@ var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 var productsRouter = require("./routes/products");
 var commentsRouter = require("./routes/comments");
+var authRouter = require("./routes/auth");
+var authenticate = require("./middleware/auth");
 var app = express();
 
 // view engine setup
@@ -23,6 +25,10 @@ app.use("/", indexRouter);
 app.use("/users", usersRouter);
 app.use("/products", productsRouter);
 app.use("/comments", commentsRouter);
+app.use("/auth", authRouter);
+app.use("/models/products", authenticate, productsRouter);
+app.use("/models/comments", authenticate, commentsRouter);
+app.use("/models/users", authenticate, usersRouter);
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
