d1f096c52a40766bd511e3ed6fd3736227933771
Solans <solanshnd@gmail.com>
2025-07-22 13:29:55 -0400

    [FIX] Fix app.js

--- src/app.js
+++ src/app.js
@@ -19,7 +19,7 @@ app.set("view engine", "jade");
 //For web
 app.use(cors({
     origin: 'http://localhost:4200',  // Permitir acceso desde Angular
-    methods: ['GET', 'POST'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
