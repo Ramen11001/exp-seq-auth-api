240501ab9a407a22b9195505a030c1ec6ef22042
Ramen11001 <soltashnd@gmail.com>
2025-07-30 17:34:54 +0200

    [FTR] A feature was implemented to retrieve product reviews. IT DOESN'T WORK.

--- src/routes/comments.js
+++ src/routes/comments.js
@@ -67,6 +67,31 @@ router.get("/:id", async (req, res) => {
 });
 
 /**
+ * Get comments for a specific product
+ * @route GET /comments/product/:productId
+ */
+router.get('/:productId', filterPagination, async (req, res) => {
+  try {
+    const whereClause = req.query.productId ? { productId: req.query.productId } : {};  
+    const comments = await commentService.getComments({
+      ...req.queryOptions,
+      where: whereClause,
+      include: [
+        {
+          model: User,
+          attributes: ['id', 'username']
+        }
+      ]
+    });
+
+    res.json(comments); 
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).json({ error: "Error al obtener los comentarios" });
+  }
+});
+
+/**
  * Route handler for updating a comment by ID.
  * Validates the request body and returns a 404 error if the comment is not found.
  *
